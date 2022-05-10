4731b5ee9f2c903f37b19c718e0427acff04588b
root <martin.liporace@gmail.com>
2022-05-10 10:38:01 -0700

    read.c

---
+++ read.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "monty.h"
+
+ssize_t _read(const char *filename)
+ {
+   FILE *fp;
+   int c;
+  
+   fp = fopen(filename,"r");
+   while(1) {
+      c = fgetc(fp);
+      if( feof(fp) ) { 
+         break ;
+      }
+	printf("%c", c);	
+      
+   }
+
+   fclose(fp);
+   
+   return(0);
+}
+
+
+int main(void)
+
+{
+
+_read("00.m");
+return(0);
+}
