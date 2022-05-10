d20a4fa1dbf230a7036504e8691c75e6cb73f581
root <martin.liporace@gmail.com>
2022-05-10 10:40:17 -0700

    _push.c

---
+++ _push.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+* add_dnodeint - adds a new node at the beginning of a listint_t list.
+* @head: head
+* @n: n
+*
+* Return: the address of the new element or NULL if it failed
+*/
+
+stack_t *_push(stack_t **head, const int n)
+
+{
+
+	stack_t *new_node;
+
+	if (!head)
+		return (NULL);
+
+	new_node = malloc(sizeof(stack_t));
+
+	if (!new_node)
+
+		return (NULL);
+	new_node->n = n;
+	new_node->prev = NULL;
+
+	if (*head)
+		{
+		(*head)->prev = new_node;
+		new_node->next = *head;
+		}
+	else
+		new_node->next = NULL;
+	*head = new_node;
+	return (new_node);
+}
